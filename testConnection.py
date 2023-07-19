0c2d0dd47db501454f2188a75ce644e11699b687
JasonChen2118 <694593452@qq.com>
2023-07-19 19:23:28 +0800

    Simple connection availability test

--- testConnection.py
+++ testConnection.py
@@ -1,10 +1,15 @@
+import requests
+
+
 def web_test():
-    pass
+    try:
+        requests.get(url="https://www.pixiv.net/")
+    except Exception:
+        return False
+    else:
+        return True
 
 
-def main():
-    pass
-
-
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+    isAvailable = web_test()
+    print(isAvailable)
