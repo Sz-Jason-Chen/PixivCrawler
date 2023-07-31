a60c7810169aac97d24ff7eac8073fec3a50e4a4
JasonChen2118 <694593452@qq.com>
2023-07-31 12:21:21 +0800

    FileAccess create

--- fileAccess.py
+++ fileAccess.py
@@ -1,15 +1,31 @@
+import os
+
+
 class FileAccess:
     def __init__(self, file_name):
         self.file_name = file_name
+        self.output_path = os.getcwd() + "\\output\\"
+        if not os.path.exists(self.output_path):
+            os.mkdir(self.output_path)
 
 
 class TXTRead(FileAccess):
     pass
 
 
-class TXTSave(FileAccess):
-    def __init__(self, file_name):
-        super().__init__(file_name=file_name)
+class FormattedInfoSave(FileAccess):
+    def __init__(self, file_name, info):
+        super().__init__(file_name)
+        file = open(self.output_path + self.file_name, "w", encoding="UTF-8")
+        for attr in info:
+            line = attr + ": " + str(info[attr]) + "\n"
+            file.write(line)
+        file.close()
 
 
-
+class PicSave(FileAccess):
+    def __init__(self, file_name, pic):
+        super().__init__(file_name)
+        file = open(self.output_path + self.file_name, "wb")
+        file.write(pic)
+        file.close()
--- simpleCrawler.py
+++ simpleCrawler.py
@@ -1,12 +1,7 @@
-import crawler
-import time
-import random
-import json
-from config import *
+from fileAccess import *
 from text import *
 
 """
-Only for feasibility testing.
 No multithreading, might be very slow.
 """
 
@@ -15,27 +10,18 @@ def get_artwork_information(pid):
     """
     Using pixiv artwork ID, search for corresponding artwork information.
     The information is stored in a list and returned and saved as a txt file.
-    If there's no artwork under the ID, it will return an empty list and will not create a file.
 
     :param pid: Pixiv artwork ID
-    :return: A list containing detailed information of the artwork.
+    :return: Detailed information of the artwork, if ArtworkUnavailableError return None
 
     """
     pid = str(pid)
-    try:
-        text = IllustText(raw=crawler.illusts_text(pid=pid))
-    except ArtworkUnavailableError:
-        pass
-    else:
-        print(text.get_title())
-        info = text.get_text()
-        file_name = OUTPUT_PATH + pid + ".txt"
-        file = open(file_name, "w", encoding="UTF-8")
-        for attr in info:
-            line = attr + ": " + str(info[attr]) + "\n"
-            file.write(line)
-        file.close()
-        return info
+    text = IllustText(raw=crawler.illusts_text(pid=pid))
+    print(text.get_title())
+    info = text.get_text()
+    file_name = pid + ".txt"
+    FormattedInfoSave(file_name=file_name, info=info)
+    return info
 
 
 def get_artwork_picture(pid):
@@ -46,35 +32,31 @@ def get_artwork_picture(pid):
     :return:
     """
     urls = IllustPageText(raw=crawler.illust_pages_text(pid=pid)).get_original()
-
     p = 0
     for url in urls:
         print(url)
         pic = crawler.img_original_content(url)
-        file_name = OUTPUT_PATH + pid + "_p" + str(p) + ".png"
-        file = open(file_name, "wb")
-        file.write(pic)
-        file.close()
-
+        file_name = pid + "_p" + str(p) + ".png"
+        PicSave(file_name=file_name, pic=pic)
         p = p + 1
 
 
 def main():
-    pass
+    """lower = int(input("Input pid lower bound:"))
+        upper = int(input("Input pid upper bound:"))
+        for pid in range(lower, upper + 1):
+            pid = str(pid)
+            print(pid)
+            info = get_artwork_information(pid=pid)
+            if len(info) != 0:
+                get_artwork_picture(pid=pid)
+            time.sleep(random.uniform(0, 1))"""
+
+    pid = input("PID: ")
+    if get_artwork_information(pid=pid):
+        get_artwork_picture(pid=pid)
 
 
 if __name__ == "__main__":
-    """lower = int(input("Input pid lower bound:"))
-    upper = int(input("Input pid upper bound:"))
-    for pid in range(lower, upper + 1):
-        pid = str(pid)
-        print(pid)
-        info = get_artwork_information(pid=pid)
-        if len(info) != 0:
-            get_artwork_picture(pid=pid)
-        time.sleep(random.uniform(0, 1))"""
+    main()
 
-    pid = "20"
-    info = get_artwork_information(pid=pid)
-    if len(info) != 0:
-        get_artwork_picture(pid=pid)
